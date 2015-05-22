b26c6c972b8637fa58a2c858b75d428c75457427
Samuel Bishop <techdragon@Sams-MacBook-Air.local>
2015-05-22 11:46:14 +0800

    Cleaning up the base pyenv image.

--- base/bashrc.bash
+++ base/bashrc.bash
@@ -1,4 +1,4 @@
-export PYENV_ROOT="/root/.pyenv"
+export PYENV_ROOT="/python/.pyenv"
 export PATH="$PYENV_ROOT/shims:$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
